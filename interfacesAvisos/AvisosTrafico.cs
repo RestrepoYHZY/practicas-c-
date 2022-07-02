35f45b3f52ec23828efb46b6d83f0ba92db73c6d
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-02 16:38:00 -0500

    Practica interfacesAvisos

---
+++ interfacesAvisos/AvisosTrafico.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacesAvisos
+{
+   
+
+    class AvisosTrafico : IAvisos
+    {
+        public AvisosTrafico()
+        {
+            remitente = "DGT";
+
+            mensaje = "Sancion Cometida. Pague antes de 3 dias y obtendra beneficios";
+            
+            fecha = "";
+
+        }
+
+        public AvisosTrafico(String remitente, string mensaje, string fecha)
+        {
+            this.remitente = remitente;
+            this.mensaje = mensaje;
+            this.fecha = fecha;
+        }
+
+
+        public string getFecha()
+        {
+            return fecha;
+        }
+
+        public void mostrarAviso()
+        {
+            Console.WriteLine("Mensaje: {0} ha sido enviado por {1} el dia {2}",mensaje, remitente, fecha);
+        }
+
+        private string remitente;
+        private string mensaje;
+        private string fecha;
+    }
+}
---
+++ interfacesAvisos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacesAvisos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisosTrafico aviso1 = new AvisosTrafico();
+            aviso1.mostrarAviso();
+
+            AvisosTrafico aviso2 = new AvisosTrafico("Jefatura Colombia", "Sancion de velocidad", "02/07/2022");
+            Console.WriteLine(aviso2.getFecha()); 
+            aviso2.mostrarAviso();
+        }
+
+    }
+}
