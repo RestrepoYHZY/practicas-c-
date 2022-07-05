db8a2e36052fb38d65b7599994236df95be5e1bb
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-05 11:35:01 -0500

    Genericos parte2 (Con restriccion)

---
+++ GenericosRestricciones/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericosRestricciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmacenEmpleados<Director> empleados = new AlmacenEmpleados<Director>(2);
+
+            empleados.agregar(new Director(1200));
+            empleados.agregar(new Director(2200));
+        }
+    }
+
+    class AlmacenEmpleados<T> where T: IParaEmpleados
+    {
+        public AlmacenEmpleados(int z)
+        {
+            datosEmpleados = new T[z]; 
+        }
+
+        public void agregar(T obj)
+        {
+            datosEmpleados[i]=obj;
+            i++;
+        }
+
+        public T getEmpleado(int i)
+        {
+            return datosEmpleados[i];
+        } 
+
+        private int i = 0;
+        private T[] datosEmpleados;
+    }
+
+
+
+    class Director:IParaEmpleados
+    {
+        public Director (double salario)
+        {
+            this.salario = salario;
+        }
+
+        private double salario; 
+        public double getSalario()
+        {
+            return salario;
+        }
+    }
+
+    class Secretaria:IParaEmpleados
+    {
+        public Secretaria(double salario)
+        {
+            this.salario = salario;
+        }
+
+        private double salario;
+        public double getSalario()
+        {
+            return salario;
+        }
+
+    }
+
+    class Electricista: IParaEmpleados
+    {
+        public Electricista(double salario)
+        {
+            this.salario = salario;
+        }
+
+        private double salario;
+        public double getSalario()
+        {
+            return salario;
+        }
+    }
+
+    interface IParaEmpleados
+    {
+        double getSalario();
+    }
+}
