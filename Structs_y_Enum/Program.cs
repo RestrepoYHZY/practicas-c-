005ffb6ff10a236564af104a9c826194da9c6984
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-04 21:27:01 -0500

    Trabajo con enum

---
+++ Structs_y_Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_y_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado(Bonus.extra, 1900.50);
+
+
+           Console.WriteLine( "El salario del empleado es: "+empleado1.getSalario());
+        }
+    }
+
+    class Empleado
+    {
+        private double salario,bonus;
+
+        public Empleado(Bonus bonusEmpleado, double salario)
+        {
+            bonus = (double)bonusEmpleado;
+
+            this.salario = salario;
+
+        }
+
+        public double getSalario()
+        {
+            return salario + bonus;
+        }
+    }
+
+  enum Bonus { bajo=500, normal=1000, bueno=1500, extra=3000};
+
+}
