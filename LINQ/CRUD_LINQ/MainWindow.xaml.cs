3dea839b23e2f1e03de5acc6e203e979ec1423c6
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-14 10:25:13 -0500

    Ejercicios de LINQ

---
+++ LINQ/CRUD_LINQ/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace CRUD_LINQ
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string miConexion = ConfigurationManager.ConnectionStrings["CRUD_LINQ.Properties.Settings.CruedLinqSql"].ConnectionString;
+
+            dataContext = new DataClasses1DataContext(miConexion);
+
+            //InsertarEmpresas();
+            //InsertarEmpleados();
+            //InsertarCargos();
+            //InsertaEmpleadoCargo();
+            // ActualizaEmpleado();
+            EliminaEmpleado();
+
+
+
+        }
+
+        public void InsertarEmpresas()
+        {
+           // dataContext.ExecuteCommand("delete from Empresa"); //Eliminar
+
+            empresa pildorasInformaticas = new empresa();
+
+            pildorasInformaticas.Nombre = "Pildoras Informaticas";
+            dataContext.empresa.InsertOnSubmit(pildorasInformaticas);
+
+            empresa empresaGoogle = new empresa();
+
+            empresaGoogle.Nombre = "Google";
+            dataContext.empresa.InsertOnSubmit(empresaGoogle);
+
+
+            dataContext.SubmitChanges();
+
+            Principal.ItemsSource = dataContext.empresa;
+
+
+        }
+
+
+        public void InsertarEmpleados()
+        {
+            empresa pildorasInformaticas = dataContext.empresa.First(em => em.Nombre.Equals("Pildoras Informaticas"));
+            empresa empresaGoogle = dataContext.empresa.First(em => em.Nombre.Equals("Google"));
+
+            List<empleado> listaEmpleados = new List<empleado>();
+
+            listaEmpleados.Add(new empleado { Nombre = "Juan", Apellido = "Beltran", EmpresaId = pildorasInformaticas.Id });
+            listaEmpleados.Add(new empleado { Nombre = "Jose", Apellido = "Ramirez", EmpresaId = pildorasInformaticas.Id });
+            listaEmpleados.Add(new empleado { Nombre = "Anna", Apellido = "Perez", EmpresaId = empresaGoogle.Id });
+            listaEmpleados.Add(new empleado { Nombre = "Maria", Apellido = "Altez", EmpresaId = empresaGoogle.Id });
+        
+            dataContext.empleado.InsertAllOnSubmit(listaEmpleados);
+
+            dataContext.SubmitChanges();
+
+            Principal.ItemsSource = dataContext.empleado;
+
+
+        }
+
+
+        public void InsertarCargos()
+        {
+            dataContext.cargo.InsertOnSubmit(new cargo { NombreCargo = "Ddirector/a" });
+            dataContext.cargo.InsertOnSubmit(new cargo { NombreCargo = "Administrativo/a" });
+
+            dataContext.SubmitChanges();
+
+            Principal.ItemsSource = dataContext.cargo;
+
+        }
+
+        public void InsertaEmpleadoCargo()
+        {
+            empleado Juan = dataContext.empleado.First(em => em.Nombre.Equals("Juan"));
+            empleado Jose = dataContext.empleado.First(em => em.Nombre.Equals("Anna"));
+            empleado Anna = dataContext.empleado.First(em => em.Nombre.Equals("Anna"));
+            empleado Maria= dataContext.empleado.First(em => em.Nombre.Equals("Anna"));
+
+
+            cargo cDirector = dataContext.cargo.First(cg => cg.NombreCargo.Equals("Ddirector/a"));
+            cargo cAdtvo = dataContext.cargo.First(cg => cg.NombreCargo.Equals("Administrativo/a"));
+
+           /* cargoEmpleado cargoJuan = new cargoEmpleado();
+
+            cargoJuan.empleado = Juan;
+            cargoJuan.CargoId = cDirector.Id;*/
+
+
+            List<cargoEmpleado> listaCargosEmpleados = new List<cargoEmpleado>();
+
+            listaCargosEmpleados.Add(new cargoEmpleado { empleado = Juan, cargo = cDirector });
+            listaCargosEmpleados.Add(new cargoEmpleado { empleado = Jose, cargo = cAdtvo });
+            listaCargosEmpleados.Add(new cargoEmpleado { empleado = Anna, cargo = cDirector });
+            listaCargosEmpleados.Add(new cargoEmpleado { empleado = Maria, cargo = cAdtvo});
+
+
+            dataContext.cargoEmpleado.InsertAllOnSubmit(listaCargosEmpleados);
+
+            dataContext.SubmitChanges();
+            Principal.ItemsSource = dataContext.cargoEmpleado;
+
+            
+        }
+
+
+        public void ActualizaEmpleado()
+        {
+            empleado Maria = dataContext.empleado.First(em => em.Nombre.Equals("Maria"));
+
+            Maria.Nombre = "Maria Angeles";
+
+            dataContext.SubmitChanges();
+            Principal.ItemsSource = dataContext.empleado;
+
+
+        }
+
+        public void EliminaEmpleado()
+        {
+            empleado Juan = dataContext.empleado.First(em => em.Nombre.Equals("Juan"));
+       
+            dataContext.empleado.DeleteOnSubmit(Juan);
+
+            dataContext.SubmitChanges();
+            Principal.ItemsSource = dataContext.empleado;
+
+        }
+    }
+}
---
+++ LINQ/LINQ2/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ControlEmpresasEmpleados ce= new ControlEmpresasEmpleados();
+
+            /*ce.getCeo();
+            Console.WriteLine(" ");
+            ce.getSalario();
+            Console.WriteLine(" ");
+            ce.getEmpleadosOrdenados();
+            Console.WriteLine("");*/
+
+            string entrada = Console.ReadLine();
+            try
+            {
+                int entradaId = Convert.ToInt32(entrada);
+
+                ce.getEmpleadosEmpresa(entradaId);
+            }
+            catch (Exception)
+            {
+                  Console.WriteLine("Has introducido un Id erróneo");
+            }
+        }
+    }
+
+    class ControlEmpresasEmpleados
+    {
+        public ControlEmpresasEmpleados()
+        {
+            listaEmpresas = new List<Empresa>();
+            listaEmpleados = new List<Empleado>();
+
+            listaEmpresas.Add(new Empresa { Id = 1, Nombre = "Google" });
+            listaEmpresas.Add(new Empresa { Id = 2, Nombre = "Pildoras Informaticas" });
+
+            listaEmpleados.Add(new Empleado { Id = 1, Nombre = "Yhaizley RV", Cargo = "Ceo", Salario = 150000, EmpresaId = 1 });
+            listaEmpleados.Add(new Empleado { Id = 2, Nombre = "Juan David", Cargo = "Ceo", Salario = 180000, EmpresaId = 2 });
+            listaEmpleados.Add(new Empleado { Id = 3, Nombre = "Margot", Cargo = "Co-Ceo", Salario = 1100, EmpresaId = 1 });
+           
+
+        
+        }
+
+        public void getCeo()
+        {
+            IEnumerable<Empleado> ceos = from empleado in listaEmpleados where empleado.Cargo == "Ceo" select empleado;
+
+            foreach (Empleado empleado1 in ceos)
+            {
+                empleado1.getDatosEmpleado();
+                
+            }
+        
+        }
+
+
+
+        public void getSalario()
+        {
+            IEnumerable<Empleado> salario = from elSalario in listaEmpleados where elSalario.Salario < 100000 select elSalario; 
+
+            foreach(Empleado salario1 in salario)
+            {
+                salario1.getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadosOrdenados()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados orderby empleado.Nombre select empleado;
+
+            foreach (Empleado empleado1 in empleados)
+            {
+                empleado1.getDatosEmpleado();
+
+            }
+        }
+
+        public void getEmpleadosEmpresa(int Id)
+        {
+            IEnumerable<Empleado> empleadosPil = from empleado in listaEmpleados join empresa in listaEmpleados
+                                              on empleado.EmpresaId equals empresa.Id
+                                              where empresa.Id==Id
+                                              select empleado;
+
+
+
+            foreach (Empleado empleado1 in empleadosPil)
+            {
+                empleado1.getDatosEmpleado();
+
+            }
+        }
+
+
+
+
+        public List<Empresa> listaEmpresas;
+        public List<Empleado> listaEmpleados;
+    }
+
+
+    class Empresa
+    {
+        public int Id { get; set; }
+
+        public string Nombre{ get; set; }
+
+        public void getDatosEmpresa()
+        {
+            Console.WriteLine("Empresa {0} con Id {1}", Nombre, Id);
+        }
+
+    }
+
+
+    class Empleado
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Cargo { get; set; }
+
+        public double Salario { get; set; }
+
+        //Clave Foranea
+        public int EmpresaId { get; set; }
+
+        public void getDatosEmpleado()
+        {
+            Console.WriteLine("Empleado {0} con Id {1}, cargo {2} con salario {3}  pertenece a la empresa {4}", Nombre, Id, Cargo, Salario, EmpresaId);
+
+        }
+
+    }
+}
+
+
