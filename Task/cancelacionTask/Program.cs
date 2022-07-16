e5e31d41eb91d4b376725eb2e8995539ba112250
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-16 12:18:06 -0500

    Task cancel  y parallel

---
+++ Task/cancelacionTask/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace cancelacionTask
+{
+    internal class Program
+    {
+        static int acumulador = 0;
+        static void Main(string[] args)
+        {
+            CancellationTokenSource miToken = new CancellationTokenSource();
+            CancellationToken cancelaToken = miToken.Token;
+
+
+            Task tara = Task.Run(() => RealizarTarea(cancelaToken));
+
+            for(int i=0; i<100; i++)
+            {
+                acumulador += 30;
+
+                Thread.Sleep(1000);
+
+                if (acumulador > 100)
+                {
+
+                    miToken.Cancel();
+                    break;
+                } 
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine("VALOR DEL ACUMULADOR: "+acumulador);
+            Console.ReadLine();
+            
+        }
+        
+        static void RealizarTarea(CancellationToken token)
+        {
+            for (int i=0; i<100; i++)
+            {
+                acumulador++;
+
+                var miThread = Thread.CurrentThread.ManagedThreadId;
+                Thread.Sleep(1000);
+                Console.WriteLine("Ejecutando tarea el thread: "+miThread);
+                Console.WriteLine(acumulador);
+
+                if (token.IsCancellationRequested)
+                {
+                    acumulador = 0;
+                    return; 
+                }
+            }
+
+
+        }
+    }
+
+}
---
+++ Task/claseParallel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace claseParallel
+{ 
+
+    internal class Program
+    {
+
+        private static int acumulador = 0;
+        static void Main(string[] args)
+        {
+
+            /* for(int i=0; i<100; i++)
+             {
+                 RealizarTarea(i);
+
+                 Console.WriteLine($"Acumulador vale {acumulador}. Tarea realizada por el hilo {Thread.CurrentThread.ManagedThreadId}");
+             }*/
+
+            Parallel.For(0, 100, RealizarTarea);
+        }
+          
+        static void RealizarTarea(int dato)
+        {
+            Console.WriteLine($"Acumulador vale {acumulador}. Tarea realizada por el hilo {Thread.CurrentThread.ManagedThreadId}");
+            if ((acumulador % 2) == 0)
+            {
+                acumulador += dato;
+
+                Thread.Sleep(100);
+            }
+            else
+            {
+                acumulador -= dato;
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
